015f0770bbfa6244b250cbb90a4b24d32b225c0e
David Lesieur <david@davidlesieur.com>
2023-02-03 16:41:15 -0500

    Fix year validation with non-number strings.

--- ckanext/desq/validators.py
+++ ckanext/desq/validators.py
@@ -1,14 +1,15 @@
 """Custom validators for DESQ."""
 
 from ckan.common import _
-from ckan.logic.validators import is_positive_integer
+from ckan.logic.validators import int_validator, is_positive_integer
 from ckan.plugins.toolkit import Invalid
 
 
 def is_year(value, context):
-    value = is_positive_integer(value, context)
-    if value < 1900:
-        raise Invalid(_("Must be greater than 1900"))
-    if value > 2100:
-        raise Invalid(_("Must be less than 2100"))
+    value = int_validator(value, context)
+    if value is not None:
+        if value < 1900:
+            raise Invalid(_("Must be greater than 1900"))
+        if value > 2100:
+            raise Invalid(_("Must be less than 2100"))
     return value
