f32f6d3c5dc00de78a34d90d064217660a63ee5b
David Lesieur <david@davidlesieur.com>
2023-02-05 02:50:57 -0500

    Update dataset schema.

--- ckanext/desq/plugin.py
+++ ckanext/desq/plugin.py
@@ -1,15 +1,19 @@
 """Plugin interfaces definitions."""
 
+import json
+
 from ckan import plugins
 from ckan.plugins import toolkit
 
 from ckanext.desq import validators as desq_validators
 
 
-class DesqPlugin(plugins.SingletonPlugin, toolkit.DefaultDatasetForm):
+class DesqPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IPluginObserver)
     plugins.implements(plugins.IValidators)
     plugins.implements(plugins.IFacets)
+    plugins.implements(plugins.IPackageController)
 
     # IConfigurer
 
@@ -24,10 +28,40 @@ class DesqPlugin(plugins.SingletonPlugin, toolkit.DefaultDatasetForm):
     def get_validators(self):
         return {
             'desq_is_year': desq_validators.is_year,
+            'desq_tag_convert': desq_validators.tag_convert,
+            'desq_no_future_date': desq_validators.no_future_date,
         }
 
+    # IPluginObserver
+
+    @staticmethod
+    def _create_vocabulary(vocabulary_id):
+        """Create the specified tag vocabulary if it does not already exists."""
+        # Guide: https://docs.ckan.org/en/2.9/extensions/adding-custom-fields.html#tag-vocabularies
+        user = toolkit.get_action('get_site_user')({'ignore_auth': True}, {})
+        context = {'user': user['name']}
+        try:
+            data = {'id': vocabulary_id}
+            toolkit.get_action('vocabulary_show')(context, data)
+        except toolkit.ObjectNotFound:
+            data = {'name': vocabulary_id}
+            return toolkit.get_action('vocabulary_create')(context, data)
+
+    def before_load(self, plugin):
+        pass
+
+    def after_load(self, service):
+        # Make sure the required vocabularies exist.
+        self._create_vocabulary('dimension')
+
+    def before_unload(self, plugin):
+        pass
+
+    def after_unload(self, service):
+        pass
+
     # IFacets
-    # Reference: https://docs.ckan.org/en/2.9/extensions/plugin-interfaces.html?highlight=ifacet#ckan.plugins.interfaces.IFacets
+    # Reference: https://docs.ckan.org/en/2.9/extensions/plugin-interfaces.html#ckan.plugins.interfaces.IFacets
 
     def dataset_facets(self, facets_dict, package_type):
         """Modify and return the facets_dict for the dataset search page."""
@@ -41,3 +75,108 @@ class DesqPlugin(plugins.SingletonPlugin, toolkit.DefaultDatasetForm):
     def organization_facets(self, facets_dict, organization_type, package_type):
         """Modify and return the facets_dict for an organization’s page."""
         return facets_dict
+
+    # IPackageController
+    # Reference: https://docs.ckan.org/en/2.9/extensions/plugin-interfaces.html#ckan.plugins.interfaces.IPackageController
+
+    def read(self, entity):
+        """Called after IPackageController.before_view inside package_show."""
+        pass
+
+    def create(self, entity):
+        """Called after the dataset had been created inside package_create."""
+        pass
+
+    def edit(self, entity):
+        """Called after the dataset had been updated inside package_update."""
+        pass
+
+    def delete(self, entity):
+        """Called before commit inside package_delete."""
+        pass
+
+    def after_create(self, context, pkg_dict):
+        """
+        Extensions will receive the validated data dict after the dataset
+        has been created (Note that the create method will return a dataset
+        domain object, which may not include all fields). Also the newly
+        created dataset id will be added to the dict.
+        """
+        pass
+
+    def after_update(self, context, pkg_dict):
+        """
+        Extensions will receive the validated data dict after the dataset
+        has been updated.
+        """
+        pass
+
+    def after_delete(self, context, pkg_dict):
+        """
+        Extensions will receive the data dict (typically containing
+        just the dataset id) after the dataset has been deleted.
+        """
+        pass
+
+    def after_show(self, context, pkg_dict):
+        """
+        Extensions will receive the validated data dict after the dataset
+        is ready for display.
+        """
+        pass
+
+    def before_search(self, search_params):
+        """
+        Extensions will receive a dictionary with the query parameters,
+        and should return a modified (or not) version of it.
+
+        search_params will include an `extras` dictionary with all values
+        from fields starting with `ext_`, so extensions can receive user
+        input from specific fields.
+        """
+        return search_params
+
+    def after_search(self, search_results, search_params):
+        """
+        Extensions will receive the search results, as well as the search
+        parameters, and should return a modified (or not) object with the
+        same structure::
+
+            {'count': '', 'results': '', 'facets': ''}
+
+        Note that count and facets may need to be adjusted if the extension
+        changed the results for some reason.
+
+        search_params will include an `extras` dictionary with all values
+        from fields starting with `ext_`, so extensions can receive user
+        input from specific fields.
+        """
+        return search_results
+
+    def before_index(self, data_dict):
+        """
+        Process data before passing it to Solr.
+
+        Extensions will receive what will be given to Solr for
+        indexing. This is essentially a flattened dict (except for
+        multi-valued fields such as tags) of all the terms sent to
+        the indexer. The extension can modify this by returning an
+        altered version.
+        """
+
+        # TODO: Index full strings of multiple_select fields.
+
+        # TODO: Remove the scheming_nerf_index plugin, and handle repeating subfields properly.
+        # References:
+        # - https://github.com/ckan/ckanext-scheming#repeating_subfields
+        # - https://ckan.org/blog/scheming-subfields
+
+        return data_dict
+
+    def before_view(self, data_dict):
+        """
+        Extensions will receive this before the dataset gets
+        displayed. The dictionary passed will be the one that gets
+        sent to the template.
+        """
+        return data_dict
--- ckanext/desq/validators.py
+++ ckanext/desq/validators.py
@@ -1,8 +1,14 @@
 """Custom validators for DESQ."""
 
+from datetime import datetime
+from itertools import count
+
 from ckan.common import _
-from ckan.logic.validators import int_validator, is_positive_integer
+from ckan.logic.validators import (int_validator, is_positive_integer,
+                                   tag_length_validator, tag_name_validator)
 from ckan.plugins.toolkit import Invalid
+
+from ckanext.desq import query
 
 
 def is_year(value, context):
@@ -12,4 +18,38 @@ def is_year(value, context):
             raise Invalid(_("Must be greater than 1900"))
         if value > 2100:
             raise Invalid(_("Must be less than 2100"))
+    return value
+
+
+def tag_convert(key, data, errors, context):
+    """
+    Parse comma-separated tags, validate and add them to the data dict.
+
+    For each tag, a vocabulary id is selected based on the `key` argument.
+    """
+    # Derived from ckan.logic.validators.tag_string_convert().
+
+    # Note: This assumes that the vocabulary name is the same as the field key!
+    vocabulary = query.get_vocabulary_by_name(context, key)
+
+    if isinstance(data[key], str):
+        tags = [tag.strip() for tag in data[key].split(',') if tag.strip()]
+    else:
+        tags = data[key]
+
+    current_index = max([int(k[1]) for k in data.keys() if len(k) == 3 and k[0] == 'tags'] + [-1])
+
+    for num, tag in zip(count(current_index + 1), tags):
+        data[('tags', num, 'name')] = tag
+        data[('tags', num, 'vocabulary_id')] = vocabulary.id if vocabulary else None
+
+    for tag in tags:
+        tag_length_validator(tag, context)
+        tag_name_validator(tag, context)
+
+
+def no_future_date(key, data, errors, context):
+    value = data.get(key)
+    if value and value > datetime.today():
+        raise Invalid(_("Date may not be in the future."))
     return value
